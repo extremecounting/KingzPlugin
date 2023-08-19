aeb230e922f197edcbe425113b658a583aa2585e
Delsin <44517733+xDelsin@users.noreply.github.com>
2023-08-18 23:45:56 -0400

    Update Commands.java

--- src/main/java/com/extremecounting/dungeon/Commands.java
+++ src/main/java/com/extremecounting/dungeon/Commands.java
@@ -39,8 +39,64 @@ public class Commands implements CommandExecutor {
             player.getLocation().getBlock().setType(Material.CHEST);
             Chest chest = (Chest) player.getLocation().getBlock().getState();
             chest.getInventory().addItem(new ItemStack(Material.STONE));
-        }
+        } else if (cmd.getName().equalsIgnoreCase("tpa")) {
+            if (args.length != 1) {
+                player.sendMessage(ChatColor.RED + "Usage: /tpa <player>");
+                return true;
+            }
+            Player targetPlayer = player.getServer().getPlayer(args[0]);
+            if (targetPlayer == null || !targetPlayer.isOnline()) {
+                player.sendMessage(ChatColor.RED + "Player not found or offline.");
+                return true;
+            }
+            player.sendMessage(ChatColor.GOLD + "Request sent to: " + targetPlayer.getName());
+            targetPlayer.sendMessage(ChatColor.YELLOW + player.getName() +" wants to teleport to you! Click " + ChatColor.GREEN + "ACCEPT" + ChatColor.YELLOW + " to accept or Click " + ChatColor.RED + "DENY" + ChatColor.YELLOW + " to deny. You have 30 seconds to decide.");
 
+            ComponentBuilder acceptButton = new ComponentBuilder("ACCEPT")
+                    .color(net.md_5.bungee.api.ChatColor.GREEN)
+                    .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/tpaccept " + player.getName()));
+            ComponentBuilder denyButton = new ComponentBuilder("DENY")
+                    .color(net.md_5.bungee.api.ChatColor.RED)
+                    .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/tpdeny " + player.getName()));
+            targetPlayer.spigot().sendMessage(acceptButton.create());
+            targetPlayer.spigot().sendMessage(denyButton.create());
+
+            teleportRequests.put(targetPlayer, player);
+
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    if (teleportRequests.containsKey(targetPlayer)) {
+                        teleportRequests.remove(targetPlayer);
+                        player.sendMessage(ChatColor.YELLOW + "Teleport request to " + targetPlayer.getName() + ChatColor.RED + " expired.");
+                        targetPlayer.sendMessage(ChatColor.YELLOW + "Teleport request from " + player.getName() + ChatColor.RED + " expired.");
+                    }
+                }
+            }.runTaskLater(plugin, 600L);
+        } else if (cmd.getName().equalsIgnoreCase("tpaccept")) {
+            Player accepter = player;
+            Player targetPlayer = teleportRequests.get(accepter);
+            if (targetPlayer != null) {
+                accepter.sendMessage(ChatColor.GREEN + "You accepted the teleport request from " + ChatColor.YELLOW + targetPlayer.getName() + ChatColor.GREEN + "! Teleporting in 5 seconds...");
+                targetPlayer.sendMessage(ChatColor.YELLOW + accepter.getName() + ChatColor.GREEN + " has ACCEPTED your TP request! Teleporting them in 5 seconds...");
+                new BukkitRunnable() {
+                    @Override
+                    public void run() {
+                        accepter.teleport(targetPlayer.getLocation());
+                        accepter.sendMessage(ChatColor.GREEN + "You have been teleported to " + ChatColor.YELLOW + targetPlayer.getName() + ChatColor.GREEN + "!");
+                    }
+                }.runTaskLater(plugin, 100L);
+                teleportRequests.remove(accepter);
+            }
+        } else if (cmd.getName().equalsIgnoreCase("tpdeny")) {
+            Player denier = player;
+            Player targetPlayer = teleportRequests.get(denier);
+            if (targetPlayer != null) {
+                denier.sendMessage(ChatColor.RED + "You DENIED the TP request from" + ChatColor.YELLOW + targetPlayer.getName() + ChatColor.RED + ".");
+                targetPlayer.sendMessage(ChatColor.YELLOW + denier.getName() + ChatColor.RED + " DENIED your TP request.");
+                teleportRequests.remove(denier);
+            }
+        }
 
         return true;
     }
